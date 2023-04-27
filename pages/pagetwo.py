b18674895dcf95940676e6cdd88dc2e5925c0248
Kalamojo <kojo22444488888888@gmail.com>
2023-04-26 19:32:20 -0500

    Exception handling

--- pages/pagetwo.py
+++ pages/pagetwo.py
@@ -10,15 +10,15 @@ df = pd.read_csv("./resources/standard_stats_5.csv")
 playerList = stm.multiselect("Enter Player Names", sorted(list(set(df["Player"]))))
 #player2 = stm.text_input("Enter the second player name")
 if playerList:
-	#try:
-	player_dfs = [df[df['Player']==name] for name in playerList]
-	valid_years = set(player_dfs[0]["Year"].to_list())
-	for s in player_dfs[1:]:
-		valid_years.intersection_update(s["Year"])
-	years = stm.slider('Select Year Range', min(valid_years), max(valid_years), value=[min(valid_years), max(valid_years)])
-	#print(years)
-	fig = spyder(playerList, df, "Stats Compare", set([year for year in range(years[0], years[1]+1)]))
-	stm.pyplot(fig)
+	try:
+		player_dfs = [df[df['Player']==name] for name in playerList]
+		valid_years = set(player_dfs[0]["Year"].to_list())
+		for s in player_dfs[1:]:
+			valid_years.intersection_update(s["Year"])
+		years = stm.slider('Select Year Range', min(valid_years), max(valid_years), value=[min(valid_years), max(valid_years)])
+		#print(years)
+		fig = spyder(playerList, df, "Stats Compare", set([year for year in range(years[0], years[1]+1)]))
+		stm.pyplot(fig)
 			
-	#except:
-	#	stm.error("Player not found")
+	except:
+		stm.error("Player not found")
