75ab3a89fd07411f46dcf5ae747d13ddd95d6e7e
Kalamojo <kojo22444488888888@gmail.com>
2023-04-28 23:54:31 -0500

    Restored error handling

--- pages/pageone.py
+++ pages/pageone.py
@@ -30,36 +30,36 @@ player = stm.selectbox("Enter the name of a player",
 						  [''] + sorted(list(set(df["Player"]))),
 						  0)
 if player:
-	#try:
-	player_df = df[df["Player"] == player]
-	player_df.index = player_df.pop("Year")
-	player_df.sort_index(inplace=True)
-	#print(player_df)
-	col1a, col2a = stm.columns([1, 2])
-	with col2a:
-		stm.title(player)
-		stm.caption(player_df.iloc[0]["Player Link"])
-		stm.write("Squad: " + player_df.iloc[-1]['Squad'])
-	stat = stm.selectbox("Select a stat", ['Gls', 'Ast', 'G+A', 'G-PK', 'PK', 'PKatt'])
-	col1b, col2b = stm.columns([1, 1])
-	players = [player]
-	with col1b:
-		fig = spyder1(players, df, player)
-		#mpld3.plugins.connect(fig, mpld3.plugins.PointLabelTooltip([], labels=players))
-		stm.pyplot(fig)
-		#stm.write(mpld3.fig_to_html(fig))
-		#stm.plotly_chart(fig)
-	with col2b:
-		fig = line_chart(players, df, stat)
-		stm.plotly_chart(fig)
-	player_df_rom = player_df.drop(["Rk", "Player", "Born", "Matches", "Player Link", "Player ID"], 
-									  axis=1)
-	player_df_rom.index = player_df_rom.index.map(str)
-	#print(player_df_rom)
-	stm.dataframe(player_df_rom)
-	image_link = get_img_link(player_df.iloc[0]["Player Link"])
-	with col1a:
-		stm.image(image_link)
+	try:
+		player_df = df[df["Player"] == player]
+		player_df.index = player_df.pop("Year")
+		player_df.sort_index(inplace=True)
+		#print(player_df)
+		col1a, col2a = stm.columns([1, 2])
+		with col2a:
+			stm.title(player)
+			stm.caption(player_df.iloc[0]["Player Link"])
+			stm.write("Squad: " + player_df.iloc[-1]['Squad'])
+		stat = stm.selectbox("Select a stat", ['Gls', 'Ast', 'G+A', 'G-PK', 'PK', 'PKatt'])
+		col1b, col2b = stm.columns([1, 1])
+		players = [player]
+		with col1b:
+			fig = spyder1(players, df, player)
+			#mpld3.plugins.connect(fig, mpld3.plugins.PointLabelTooltip([], labels=players))
+			stm.pyplot(fig)
+			#stm.write(mpld3.fig_to_html(fig))
+			#stm.plotly_chart(fig)
+		with col2b:
+			fig = line_chart(players, df, stat)
+			stm.plotly_chart(fig)
+		player_df_rom = player_df.drop(["Rk", "Player", "Born", "Matches", "Player Link", "Player ID"], 
+										  axis=1)
+		player_df_rom.index = player_df_rom.index.map(str)
+		#print(player_df_rom)
+		stm.dataframe(player_df_rom)
+		image_link = get_img_link(player_df.iloc[0]["Player Link"])
+		with col1a:
+			stm.image(image_link)
 			
-	#except:
-	#	stm.error("Player not found")
+	except:
+		stm.error("Player not found")
