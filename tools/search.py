a0b82a70f852d13d35d309cdc2345cf829cfd374
Kalamojo <kojo22444488888888@gmail.com>
2023-05-17 12:13:29 -0500

    New embeddings for parser

--- tools/search.py
+++ tools/search.py
@@ -18,7 +18,7 @@ lem = WordNetLemmatizer()
 punctuations = string.punctuation
 stop_words = stopwords.words('english')
 
-def preprocess_text(news):
+def preprocess_text(news, stop=True):
     """
     This function receives headlines sentence and returns clean sentence
     """
@@ -28,42 +28,43 @@ def preprocess_text(news):
     
     #Split the sentences into words
     words = list(news.split())
-    
+
+    if stop:
+        words = [w for w in words if w not in stop_words]
+    words = [''.join(x for x in w if x.isalpha()) for w in words]
     words = [lem.lemmatize(word, "v") for word in words]
     words = [w for w in words if w not in punctuations]
-    #words = [w for w in words if w not in stop_words]
-    #words = [''.join(x for x in w if x.isalpha()) for w in words]
-    
+    if stop:
+        print(words)
     return words
 
-cats_map = {'Goals scored or allowed': 'Gls',
-            'Shots on Target': 'SoT',
-            "Carries that move the ball towards the opponent's goal line at least 10 yards from its furthest point in the last six passes, or any carry into the penalty area. Excludes carries which end in the defending 50 percent of the pitch": 'PrgC',
-            'Number of times the player controlled the ball with their feet': 'Carries',
-            'Number of times a player touched the ball. Note: Receiving a pass, then dribbling, then sending a pass counts as one touch': 'Touches',
-            'Penalty Kicks Made': 'PK',
-            'Passes Completed': 'Cmp',
-            'Pass Completion percentage': 'Cmp%',
-            'Passes that directly lead to a shot (assisted shots)': 'KP',
-            "Completed passes that move the ball towards the opponent's goal line at least 10 yards from its furthest point in the last six passes, or any completed pass into the penalty area.": 'PrgP',
-            "Tackles in which the tackler's team won possession of the ball": 'TklW',
-            'number of times the goalie saves a ball from being scored': 'Saves',
-            'percentage of shots the goalie saves from scoring': 'Save%',
-            'number of goals the goalie allows to be scored against them': 'GA',
-            'Full matches by goalkeeper where no goals are allowed.': 'CS',
-            'Average length of passes, in yards': 'AvgLen',
-            'Number of crosses into penalty area which were successfully stopped by the goalkeeper': 'Stp',
-            'number of Passes Attempted': 'Att',
-            'Total number of shots attempted': 'Sh/90',
-            'Shots on Target per 90 minutes': 'SoT/90',
-            'Number of players tackled plus number of interceptions': 'Tkl+Int',
-            'Number of players tackled': 'Tkl',
-            'number of Interceptions': 'Int',
-            'Player kicking the ball away from their goal': 'Clr',
-            'number of Aerial duels': 'Won',
-            "Total distance, in yards, a player moved the ball while controlling it with their feet towards the opponent's goal": 'PrgDist',
-            'Number of times blocking a pass by standing in its path': 'Pass',
-            'number of Assists': 'Ast'
+cats_map = {'Goals scored': 'Gls',
+         'Shoots ball on Target': 'SoT',
+         'Progressive Balls Carries': 'PrgC',
+         'Ball Carries': 'Carries',
+         'Ball touches': 'Touches',
+         'Penalty Kicks': 'PK',
+         'Passes Completed': 'Cmp',
+         'Pass Completion percentage': 'Cmp%',
+         'Key passes': 'KP',
+         'Progressive completed passes': 'PrgP',
+         'Tackles won': 'TklW',
+         'Goalie saves': 'Saves',
+         'Goalie save percentage': 'Save%',
+         'Goalie misses': 'GA',
+         'No goals allowed': 'CS',
+         'Average pass length': 'AvgLen',
+         'Goalie stops crosses': 'Stp',
+         'Passes Attempted': 'Att',
+         'Attempt shoot ball': 'Sh/90',
+         'Tackles and ball interceptions': 'Tkl+Int',
+         'Tackles': 'Tkl',
+         'Intercept ball': 'Int',
+         'Cleared ball away': 'Clr',
+         'Aerial duels': 'Won',
+         'Moved ball toward': 'PrgDist',
+         'Blocked pass': 'Pass',
+         'Assists': 'Ast'
 }
 
 pages = [k for k in cats_map.keys()]
